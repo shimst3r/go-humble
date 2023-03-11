89bd5c4d0e92d906c05598560bf10d8848c44326
Nils Müller <shimst3r@gmail.com>
2023-03-11 12:29:07 +0100

    Add test for games

---
+++ pkg/feed_test.go
@@ -0,0 +1,24 @@
+package gohumble
+
+import (
+	"sync"
+	"testing"
+)
+
+func Test_updateCategory(t *testing.T) {
+	type args struct {
+		wg       *sync.WaitGroup
+		category string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"games", args{wg: &sync.WaitGroup{}, category: "games"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			updateCategory(tt.args.wg, tt.args.category)
+		})
+	}
+}
